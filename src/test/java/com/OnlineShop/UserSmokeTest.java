5e8a065ffd420c3e7f6be18968a902c31385192d
Ashwin <aaparajiya@gmail.com>
2019-10-09 10:37:29 +0100

    Add project

---
+++ src/test/java/com/OnlineShop/UserSmokeTest.java
@@ -0,0 +1,35 @@
+package com.OnlineShop;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import junit.framework.Assert;
+
+public class UserSmokeTest {
+public WebDriver driver;
+
+@BeforeMethod
+public void launchBroswer() {
+	System.setProperty("webdriver.chorme.driver","c:/chromedriver.exe");
+	driver = new ChromeDriver();
+}
+@Test (priority =1)
+public void veryifyLaunchApp() {
+	driver.get("http://www.google.co.uk");
+	Assert.assertEquals(driver.getTitle(), "Google");
+}
+@Test (priority = 2)
+public void veryifyElements() {
+	driver.get("http://www.google.co.uk");
+	Assert.assertEquals(driver.findElement(By.linkText("Gmail")).isDisplayed(), true);
+}
+	
+@AfterMethod
+public void closeBrowser() {
+	driver.close();
+}
+}
